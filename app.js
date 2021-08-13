875458a3d8d8b0dd88b1a2d4c4c6d1375cc0b08e
Tien <tmai1@babson.edu>
2021-08-13 19:58:28 +0700

    update authen with google

--- app.js
+++ app.js
@@ -38,7 +38,10 @@ mongoose.connect("mongodb://localhost:27017/userDB",
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
+
 });
 
 //silent the warning
@@ -54,42 +57,47 @@ userSchema.plugin(findorcreate);
 const User = new mongoose.model("User", userSchema)
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(User.serializeUser(function(user,done){
+    done(null,user.id)
+}));
+
+passport.deserializeUser(User.deserializeUser(function(id,done){
+    User.findById(id, function(err,user){
+        done(err,user)
+    })
+}));
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRETS,
     callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL:"https://www.googleapis.com/oath2/v3/userinfo"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    //find or create find a user find id, if not existed then it will create a new ont
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+    userProfileURL: "https://www.googleapis.com/oath2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        //find or create find a user find id, if not existed then it will create a new one
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
 ));
 
 app.get("/", function (req, res) {
     res.render("home")
 })
 
-app.get("/auth/google", function(Req,res){
+app.get("/auth/google", 
     //triggers the pop-up for users to signup with google
-    passport.authenticate("google",{scope:["profile"]})
+    passport.authenticate("google", { scope: ["profile"] })
 
-});
-
-app.get('auth/google/secrets',
-passport.authenticate('google',{failureRedirect:'/login'}),
-function(req,res){
-    //successful authentication -> direct home
-    res.redirect("/secrets")
-}
 );
 
-
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        //successful authentication -> direct home
+        res.redirect("/secrets")
+    }
+);
 
 
 
@@ -121,12 +129,51 @@ app.get("/register", function (req, res) {
 //if users are already authenticated -> secrete page
 // else (not logged in/authenticaed) -> login
 app.get("/secrets", function(req,res){
+    // if(req.isAuthenticated()){
+    //     res.render("secrets")
+    // }else{
+    //     res.redirect("/login")
+    // }
+
+    User.find({"secret":{$ne: null}}, function(err,foundUsers){
+        if(err){
+            console.log(err)
+        }else{
+            if(foundUser){
+                res.render("secrets",{userWithSecrets:foundUsers})
+            }
+        }
+    })
+})
+
+app.get("/submit", function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets")
+        res.render("submit")
     }else{
         res.redirect("/login")
     }
+
 })
+
+app.post("/subimt", function(req,res){
+    const submittedSec = req.body.secret;
+    User.findById(req.user.id, function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSec
+                foundUser.save(function(){
+                    res.redirect("/secrets")
+                })
+            }
+
+            
+        }
+    })
+
+})
+
 
 app.get("/logout", function(req,res){
     req.logout()
@@ -189,6 +236,7 @@ app.post("/login", function (req, res) {
     })
 
 })
+
 
 
 
